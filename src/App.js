3bf5f75a5ce418249f23373c0fb19bc572343574
jvsiqueira <jvts.ifpe@gmail.com>
2023-10-27 19:24:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,161 @@
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ModalComp from "./components/ModalComp";
+
+const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [data, setData] = useState([]);
+  const [dataEdit, setDataEdit] = useState({});
+
+  const isMobile = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:8080/tarefas")
+      .then((retorno) => retorno.json())
+      .then((retornoConvertidoEmJson) => setData(retornoConvertidoEmJson));
+
+    //setData(db_costumer);
+  }, [setData]);
+
+  const handleRemove = (id) => {
+    //const newArray = data.filter((item) => item.email !== email);
+
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "aplication/x-www-form-urlencoded",
+        "Access-Control-Allow-Origin": "*",
+        "Acess-Control-Allow-Methods": [
+          "GET",
+          "OPTIONS",
+          "POST",
+          "PUT",
+          "PATCH",
+          "DELETE",
+        ],
+        "Access-Control-Allow-Headers": "Content-Type,Authorization",
+        "Access-Control-Allow-Credentials": "true",
+      },
+    };
+
+    try {
+      const retorno = fetch("http://localhost:8080/tarefas/" + id, options);
+
+      if (retorno.status == 200) {
+        console.log("removido");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    fetch("http://localhost:8080/tarefas")
+      .then((retorno) => retorno.json())
+      .then((retornoConvertidoEmJson) => setData(retornoConvertidoEmJson));
+
+    window.location.reload(true);
+  };
+
+  return (
+    <Flex
+      h="100vh"
+      align="center"
+      justify="center"
+      fontSize="20px"
+      fontFamily="poppins"
+    >
+      <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
+        <Button colorScheme="blue" onClick={() => [setDataEdit({}), onOpen()]}>
+          NOVO CADASTRO
+        </Button>
+
+        <Box overflowY="auto" height="100%">
+          <Table mt="6">
+            <Thead>
+              <Tr>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                  ID
+                </Th>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                  Titulo
+                </Th>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                  Descricao
+                </Th>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                  Prazo
+                </Th>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                  Prioridade
+                </Th>
+                <Th p={0}></Th>
+                <Th p={0}></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.map(
+                ({ id, titulo, descricao, prazo, prioridade }, index) => (
+                  <Tr key={index} cursor="pointer " _hover={{ bg: "gray.100" }}>
+                    <Td maxW={isMobile ? 5 : 100}>{id}</Td>
+                    <Td maxW={isMobile ? 5 : 100}>{titulo}</Td>
+                    <Td maxW={isMobile ? 5 : 100}>{descricao}</Td>
+                    <Td maxW={isMobile ? 5 : 100}>{prazo}</Td>
+                    <Td maxW={isMobile ? 5 : 100}>{prioridade}</Td>
+                    <Td p={0}>
+                      <EditIcon
+                        fontSize={20}
+                        onClick={() => [
+                          setDataEdit({
+                            id,
+                            titulo,
+                            descricao,
+                            prazo,
+                            prioridade,
+                          }),
+                          onOpen(),
+                        ]}
+                      />
+                    </Td>
+                    <Td p={0}>
+                      <DeleteIcon
+                        fontSize={20}
+                        onClick={() => handleRemove(id)}
+                      />
+                    </Td>
+                  </Tr>
+                )
+              )}
+            </Tbody>
+          </Table>
+        </Box>
+      </Box>
+      {isOpen && (
+        <ModalComp
+          isOpen={isOpen}
+          onClose={onClose}
+          data={data}
+          setData={setData}
+          dataEdit={dataEdit}
+          setDataEdit={setDataEdit}
+        />
+      )}
+    </Flex>
+  );
+};
+
+export default App;
---
+++ src/components/ModalComp.js
@@ -0,0 +1,157 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
+  const [id] = useState(dataEdit.id || 0);
+  const [titulo, setTitulo] = useState(dataEdit.titulo || "");
+  const [descricao, setDesc] = useState(dataEdit.descricao || "");
+  const [prazo, setPrazo] = useState(dataEdit.prazo || "");
+  const [prioridade, setPriori] = useState(dataEdit.prioridade || "");
+
+  const handleSave = () => {
+    if (!titulo || !descricao || !prazo || !prioridade) return;
+
+    if (Object.keys(dataEdit).length) {
+      let dados = JSON.stringify({
+        id: id,
+        titulo: titulo,
+        descricao: descricao,
+        prazo: prazo,
+        prioridade: prioridade,
+      });
+      const options = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "aplication/x-www-form-urlencoded",
+          "Access-Control-Allow-Origin": "*",
+          "Acess-Control-Allow-Methods": [
+            "GET",
+            "OPTIONS",
+            "POST",
+            "PUT",
+            "PATCH",
+            "DELETE",
+          ],
+          "Access-Control-Allow-Headers": "Content-Type,Authorization",
+          "Access-Control-Allow-Credentials": "true",
+        },
+        body: dados,
+      };
+      try {
+        const retorno = fetch("http://localhost:8080/tarefas", options);
+
+        if (retorno.status == 200) {
+          console.log("atualizado");
+          window.location.reload(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      // data[dataEdit.index] = { id, titulo, descricao, prazo, prioridade };
+    } else {
+      let dados = JSON.stringify({
+        titulo: titulo,
+        descricao: descricao,
+        prazo: prazo,
+        prioridade: prioridade,
+      });
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "aplication/x-www-form-urlencoded",
+          "Access-Control-Allow-Origin": "*",
+          "Acess-Control-Allow-Methods": [
+            "GET",
+            "OPTIONS",
+            "POST",
+            "PUT",
+            "PATCH",
+            "DELETE",
+          ],
+          "Access-Control-Allow-Headers": "Content-Type,Authorization",
+          "Access-Control-Allow-Credentials": "true",
+        },
+        body: dados,
+      };
+      try {
+        const retorno = fetch("http://localhost:8080/tarefas", options);
+
+        if (retorno.status == 200) {
+          console.log("atualizado");
+          window.location.reload(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Cadastro de Tarefas</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormControl display="flex" flexDir="column" gap={4}>
+            <Box>
+              <FormLabel>Titulo</FormLabel>
+              <Input
+                type="text"
+                value={titulo}
+                onChange={(e) => setTitulo(e.target.value)}
+              ></Input>
+            </Box>
+            <Box>
+              <FormLabel>Descricao</FormLabel>
+              <Input
+                type="text"
+                value={descricao}
+                onChange={(e) => setDesc(e.target.value)}
+              ></Input>
+            </Box>
+            <Box>
+              <FormLabel>Prazo</FormLabel>
+              <Input
+                type="text"
+                value={prazo}
+                onChange={(e) => setPrazo(e.target.value)}
+              ></Input>
+            </Box>
+            <Box>
+              <FormLabel>Prioridade</FormLabel>
+              <Input
+                type="number"
+                value={prioridade}
+                onChange={(e) => setPriori(e.target.value)}
+              ></Input>
+            </Box>
+          </FormControl>
+        </ModalBody>
+        <ModalFooter justifyContent="start">
+          <Button colorScheme="green" mr={3} onClick={handleSave}>
+            Salvar
+          </Button>
+          <Button colorScheme="red" onClick={onClose}>
+            Cancelar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalComp;
